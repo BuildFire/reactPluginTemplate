d484c6cdd7390c79ca9efe5e29221a1525ae3197
Hiba-Almade <hibaalmade998@gmail.com>
2022-05-23 21:50:56 +0300

    add msg to layout 10

--- src/control/content/Components/Layout10/index.js
+++ src/control/content/Components/Layout10/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect , useState} from "react";
 import { hot } from "react-hot-loader/root";
 
 import useForm from "../../hooks/form";
@@ -6,6 +6,9 @@ import useForm from "../../hooks/form";
 import "./style.less";
 import "../../../../../../../styles/control/bf-base.css";
 function index(props) {
+  const [thumbnailImage, setThumbnailImage] = useState(null);
+  const [thumbnailImage2, setThumbnailImage2] = useState(null);
+
   useEffect(() => {
     let thumbnail = new buildfire.components.images.thumbnail(".thumbnail", {
       imageUrl: "",
@@ -19,19 +22,73 @@ function index(props) {
       dimensionsLabel: "Recommended: 1200 x 675",
       multiSelection: false,
     });
+    thumbnail.onChange = (imageUrl) => {
+      setThumbnailImage(imageUrl);
+    };
+    thumbnail2.onChange = (imageUrl) => {
+      setThumbnailImage2(imageUrl);
+    };
+    
+    // thumbnail Delete Image -->
+    thumbnail.onDelete = (imageUrl) => {
+      setThumbnailImage(null)
+    };
+    thumbnail2.onDelete = (imageUrl) => {
+      setThumbnailImage2(null)
+    };
+    
 
     console.log("hello", props.selectedLayout);
   }, []);
+
+  useEffect(() => {
+    setObjectData(null);
+  },[thumbnailImage ,thumbnailImage2])
+  // submit form function 
+  function submitForm(values) {
+    console.log('forms values ->', values);
+  }
+  // use hooks to make our life easier 
+  const setObjectData = (e) => {
+    let checkBoxes;
+    if (document.getElementById("enableFullScreen").checked) {
+      checkBoxes = true;
+    } else {
+      checkBoxes = false;
+    }
+    let checkBoxes2;
+    if (document.getElementById("enableFullScreen2").checked) {
+      checkBoxes2 = true;
+    } else {
+      checkBoxes2 = false;
+    }
+    let imagesObj = {
+      MainImage: thumbnailImage,
+      MainImage2: thumbnailImage2,
+      enableFullScreen1: checkBoxes,
+      enableFullScreen2: checkBoxes2,
+     
+
+      selectedLayOut: props.selectedLayout
+    }
+    if (e) {
+      handleChange(e, imagesObj);
+    } else {
+      handelChangeImage(imagesObj);
+    }
+  }
+
+
 
 
   function submitForm(values) {
     console.log('forms values ->', values);
   }
-  const { handleChange, handleSubmit } = useForm(submitForm);
+  const { handleChange, handleSubmit, handelChangeImage } = useForm(submitForm);
 
 
   return (
-    <>
+    <form onSubmit={handleSubmit}>
       <h1>Page Details</h1>
       <div className="layOutContainer">
 
@@ -48,7 +105,7 @@ function index(props) {
             <label className="lable">Enable Full Screen</label>
           </div>
           <div className="col-md-9">
-            <input type="checkBox" name="enableFullScreen" />
+            <input  onChange={setObjectData} type="checkBox" name="enableFullScreen" id="enableFullScreen" />
           </div>
         </div>
         <div className="row">
@@ -56,9 +113,9 @@ function index(props) {
             <label className="lable">Top Media Type</label>
           </div>
           <div className="col-md-9">
-            <input type="radio" name="mediaType" value="image" defaultChecked />
+            <input  onChange={setObjectData}  type="radio" name="mediaType" value="image" defaultChecked />
             <label className="lable">Image</label>
-            <input type="radio" name="mediaType" value="image" />
+            <input  onChange={setObjectData} type="radio" name="mediaType" value="video" />
             <label className="lable">Video</label>
           </div>
         </div>
@@ -76,7 +133,7 @@ function index(props) {
             <label className="lable">Enable Full Screen</label>
           </div>
           <div className="col-md-9">
-            <input type="checkBox" name="enableFullScreen" />
+            <input  onChange={setObjectData}  type="checkBox" name="enableFullScreen2" id="enableFullScreen2"/>
           </div>
         </div>
 
@@ -85,7 +142,7 @@ function index(props) {
             <label className="lable">Title</label>
           </div>
           <div className="col-md-9">
-            <input className="form-control fullWidth"></input>
+            <input  onChange={setObjectData} className="form-control fullWidth"></input>
           </div>
         </div>
         <div className="row">
@@ -93,7 +150,7 @@ function index(props) {
             <label className="lable">Subtitle</label>
           </div>
           <div className="col-md-9">
-            <input className="form-control fullWidth"></input>
+            <input  onChange={setObjectData}  className="form-control fullWidth"></input>
           </div>
         </div>
         <div className="row">
@@ -101,7 +158,7 @@ function index(props) {
             <label className="lable">Body content 1</label>
           </div>
           <div className="col-md-9">
-            <textarea className="form-control bodyContent"></textarea>
+            <textarea  onChange={setObjectData}  className="form-control bodyContent"></textarea>
           </div>
         </div>
         <div className="row margin-bottom">
@@ -109,7 +166,7 @@ function index(props) {
             <label className="lable">Body content 2</label>
           </div>
           <div className="col-md-9">
-            <textarea className="form-control bodyContent"></textarea>
+            <textarea  onChange={setObjectData}  className="form-control bodyContent"></textarea>
           </div>
         </div>
       </div>
@@ -121,7 +178,7 @@ function index(props) {
           Save
         </button>
       </div>
-    </>
+    </form>
   );
 }
 
