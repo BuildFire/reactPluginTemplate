832988b50507742a058990ea3441d8143eec63ac
ibrahemomari <ibrahem.omari96@gmail.com>
2022-05-23 16:39:27 +0300

    fix

--- src/control/content/Components/Layout1/index.js
+++ src/control/content/Components/Layout1/index.js
@@ -5,10 +5,6 @@ import "../../../../../../../styles/control/bf-base.css";
 import Design from "../../../../widget/containers/common/controllers/design.controller";
 import DesignLayoutItems from "../../../../widget/containers/common/models/design.modal";
 function index(props) {
-  const [detailsObj, setDetailsObj] = useState({});
-  const [chkbox, setChkbox] = useState(false);
-  const [isEmpty, setIsEmpty] = useState(true);
-  const [layoutId, setLayoutId] = useState("");
   useEffect(() => {
     let thumbnail = new buildfire.components.images.thumbnail(".thumbnail", {
       imageUrl: "",
@@ -16,57 +12,7 @@ function index(props) {
       dimensionsLabel: "Recommended: 675 x 1200",
       multiSelection: false,
     });
-    getData(thumbnail);
-    thumbnail.onChange = (imageUrl) => {
-      console.log("hi", imageUrl);
-      setDetailsObj({ ...detailsObj, backgroundImage: imageUrl });
-    };
   }, []);
-
-  const getData = (thumbnail) => {
-    Design.search({
-      filter: {
-        $or: [{ "$json.layout": 0 }],
-      },
-    })
-      .then((result) => {
-        if (result.length === 0) {
-          setIsEmpty(false);
-        } else {
-          thumbnail.loadbackground(result[0].data.backgroundImage);
-          setDetailsObj(result[0].data);
-          setLayoutId(result[0].id);
-        }
-      })
-      .catch((err) => console.error(err));
-  };
-
-  const saveData = () => {
-    if (isEmpty) {
-      Design.update(layoutId, detailsObj)
-        .then((result) => {
-          console.log("res", result);
-        })
-        .catch((err) => console.error(err));
-    } else {
-      let newDesign = DesignLayoutItems({detailsObj});
-      Design.insert(newDesign)
-        .then((result) => {
-          console.log("res", result);
-        })
-        .catch((err) => console.error(err));
-    }
-  };
-
-  const detailsHandle = (e) => {
-    console.log(detailsObj);
-    if (e.target.type === "checkbox") {
-      setChkbox(!chkbox);
-      setDetailsObj({ ...detailsObj, [e.target.name]: e.target.checked });
-    } else {
-      setDetailsObj({ ...detailsObj, [e.target.name]: e.target.value });
-    }
-  };
   return (
     <>
       <h1>Page Details</h1>
@@ -80,16 +26,12 @@ function index(props) {
               type="radio"
               name="mediaType"
               value="image"
-              checked={detailsObj.mediaType === "image"}
-              onChange={(e) => detailsHandle(e)}
             />
             <label className="lable">Image</label>
             <input
               type="radio"
               name="mediaType"
               value="video"
-              checked={detailsObj.mediaType === "video"}
-              onChange={(e) => detailsHandle(e)}
             />
             <label className="lable">Video</label>
           </div>
@@ -110,8 +52,6 @@ function index(props) {
             <input
               type="checkBox"
               name="enableFullScreen"
-              defaultChecked={detailsObj.enableFullScreen}
-              onChange={(e) => detailsHandle(e)}
             />
           </div>
         </div>
@@ -125,8 +65,6 @@ function index(props) {
               type="text"
               name="title"
               placeholder="Title"
-              value={detailsObj.title}
-              onChange={(e) => detailsHandle(e)}
             />
           </div>
         </div>
@@ -140,8 +78,6 @@ function index(props) {
               type="input"
               name="subtitle"
               placeholder="Subtitle"
-              value={detailsObj.subtitle}
-              onChange={(e) => detailsHandle(e)}
             />
           </div>
         </div>
@@ -153,8 +89,6 @@ function index(props) {
             <textarea
               className="form-control bodyContent"
               name="bodyContent"
-              value={detailsObj.bodyContent}
-              onChange={(e) => detailsHandle(e)}
             ></textarea>
           </div>
         </div>
@@ -166,7 +100,6 @@ function index(props) {
         <button
           className="btn btn-success"
           id="layoutSaveBtn"
-          onClick={saveData}
         >
           Save
         </button>
